46fb033cd06e970128e49465d08b5fdfd9fe0444
Roman Svistel <romansvistellviv@gmail.com>
2019-02-19 17:40:46 +0100

    Updates

--- app/js/common.js
+++ app/js/common.js
@@ -75,18 +75,21 @@ $(document).ready(function () {
                 if (up) {
                     $('.photo-half-section-tools .after')
                         .css('top', '-100%')
-                        .css('background-image', 'url("' + new_bg +'")')
-                        .animate({'top': '0'}, 500);
+                        .css('background-image', 'url("' + new_bg +'")');
+                        // .animate({'top': '-100%'}, 500);
+                    $('.photo-half-section-tools').animate({'top': '100vh'}, 500);
                 } else {
                     $('.photo-half-section-tools .after')
                         .css('top', '100%')
-                        .css('background-image', 'url("' + new_bg +'")')
-                        .animate({'top': '0'}, 500);
+                        .css('background-image', 'url("' + new_bg +'")');
+                        // .animate({'top': '100%'}, 500);
+                    $('.photo-half-section-tools').animate({'top': '-100vh'}, 500);
                 }
                 setTimeout(function () {
                     $('.photo-half-section-tools').css('background-image', 'url("' + new_bg +'")');
                     setTimeout(function () {
                         $('.photo-half-section-tools .after').remove();
+                        $('.photo-half-section-tools').css('top', '0');
                     },50);
                 }, 500);
                 $('#section-tools').removeClass(current_class).addClass(new_class).css('overflow', 'hidden');
