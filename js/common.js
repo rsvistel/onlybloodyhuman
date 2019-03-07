55fe3ab7ae01a50fcec3c8d71a4ff2aa058a8bd5
preskot <preskot53@gmail.com>
2019-03-07 20:41:02 +0200

    lazy load none

--- js/common.js
+++ js/common.js
@@ -393,7 +393,7 @@ $(document).ready(function () {
         dots: false,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
             },
             450: {
                 items: 2,
@@ -416,12 +416,12 @@ $(document).ready(function () {
     //     };
     // });
 
-    [].forEach.call(document.querySelectorAll('embed[data-src]'),    function(embed) {
-        embed.setAttribute('src', embed.getAttribute('data-src'));
-        embed.onload = function() {
-            embed.removeAttribute('data-src');
-        };
-    });
+    // [].forEach.call(document.querySelectorAll('embed[data-src]'),    function(embed) {
+    //     embed.setAttribute('src', embed.getAttribute('data-src'));
+    //     embed.onload = function() {
+    //         embed.removeAttribute('data-src');
+    //     };
+    // });
 
 });
 
