e8b02c13fdd0e2c197a02a7db6337ca8f7b891c0
preskot <preskot53@gmail.com>
2019-03-07 16:04:23 +0200

    lazy load 4

--- app/js/common.js
+++ app/js/common.js
@@ -409,12 +409,12 @@ $(document).ready(function () {
         }
     });
 
-    [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-        img.setAttribute('src', img.getAttribute('data-src'));
-        img.onload = function() {
-            img.removeAttribute('data-src');
-        };
-    });
+    // [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+    //     img.setAttribute('src', img.getAttribute('data-src'));
+    //     img.onload = function() {
+    //         img.removeAttribute('data-src');
+    //     };
+    // });
 
     [].forEach.call(document.querySelectorAll('embed[data-src]'),    function(embed) {
         embed.setAttribute('src', embed.getAttribute('data-src'));
