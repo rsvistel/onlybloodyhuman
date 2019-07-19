4f9c25d7d32b8534bd6cf21926a22db12cc4c812
romkravets <romann.kravets@gmail.com>
2019-07-19 14:54:32 +0300

    fix-script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.change-color').css('height' , '0');
     $('.text-left-logo').css('color', 'white');
+    active = 0;
 
     $('#menuToggle input, #menuToggle-mobile input').click(function () {
         if ($('body').hasClass('opened--menu')) {
@@ -14,21 +15,6 @@ $(document).ready(function () {
         $(".list-icon, .cross-icon").fadeToggle(500);
         $('.text-left-logo').css('color', 'snow');
     });
-            if ($(this).hasClass('instagram-counter')) {
-                setTimeout(function () {
-                    runSubscribersCounter()
-                }, 700);
 
-            }
-        } else {
-            for (var t = 0; t < dots.length; t++) {
-                if (dots[t].find('h6').html() === thisElement.find('h6').html()) {
-                    $('html, body').animate({scrollTop: sections[t].offset().top}, 1000);
-                }
-            }
-        }
-
-    });
-    
 });
 
