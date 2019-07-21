1d1a686b9de98624b2cfdda5ed299c6e5352db56
romkravets <romann.kravets@gmail.com>
2019-07-21 18:39:55 +0300

    scroll-offset

--- app/js/script.js
+++ app/js/script.js
@@ -14,21 +14,6 @@ $(document).ready(function () {
         $(".list-icon, .cross-icon").fadeToggle(500);
         $('.text-left-logo').css('color', 'snow');
     });
-            if ($(this).hasClass('instagram-counter')) {
-                setTimeout(function () {
-                    runSubscribersCounter()
-                }, 700);
 
-            }
-        } else {
-            for (var t = 0; t < dots.length; t++) {
-                if (dots[t].find('h6').html() === thisElement.find('h6').html()) {
-                    $('html, body').animate({scrollTop: sections[t].offset().top}, 1000);
-                }
-            }
-        }
-
-    });
-    
 });
 
