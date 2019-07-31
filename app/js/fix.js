3009e401e09c31d5876136ba96fc8249e895bc84
romkravets <romann.kravets@gmail.com>
2019-07-31 16:57:24 +0300

    fix iphoneX- responsive fullPage

--- app/js/fix.js
+++ app/js/fix.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
    var progressLine = false;
    var insta = false;
     isDesktop = true;
-    var scrolloverflowed = true;
-    // var autoScrollinged = true;
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
         || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
         isDesktop = false
@@ -48,8 +46,8 @@ $(document).ready(function () {
         anchors: ['intro', 'about', 'tools', 'skills', 'contact'],
         menu: '.dots-block-section-banner',
         css3: true,
-        scrollOverflow: scrolloverflowed,
-        responsiveWidth: 770,
+        scrollOverflow: true,
+        responsiveWidth: 1000,
         verticalCentered: false,
         allowPageScroll: true,
         lazyLoading: false,
@@ -237,6 +235,16 @@ $(document).ready(function () {
            fullpage_api.setResponsive(true);
            $.fn.fullpage.setAllowScrolling(false);
            $.fn.fullpage.setAutoScrolling(false);
+           $.fn.fullpage.setMouseWheelScrolling(false);
+        }
+      });
+      $( function() {
+        var isIPhone = window.matchMedia('(min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3)');
+        if (isIPhone.matches) {
+           fullpage_api.setResponsive(true);
+           $.fn.fullpage.setAllowScrolling(false);
+           $.fn.fullpage.setAutoScrolling(false);
+           $.fn.fullpage.setMouseWheelScrolling(false);
         }
       });
     $.fn.fullpage.setAllowScrolling(true);
@@ -430,20 +438,40 @@ $(document).ready(function () {
         setTimeout(function () { bindTools(); }, 1000);
     }
 
+    function changeTooll(current_icon, new_icon, current_class, new_class, number, up) {
+        //current_icon.animate({'opacity': '0'}, 500);
+        setTimeout(function () {
+            new_icon.css('display', 'flex').animate({'opacity': '1'}, 500);
+        }, 0);
+        if (up) {
+            $('.tool-image.active').animate({'top': '100vh'}, 500).removeClass('active');
+            $('.after-'+number)
+                .animate({'top': '0'}, 500)
+                .addClass('active');
+        } else {
+            $('.tool-image.active').animate({'top': '-100vh'}, 500).removeClass('active');
+            $('.after-'+number)
+                .animate({'top': '0'}, 500)
+                .addClass('active');
+        }
+        $('#section-tools').removeClass(current_class).addClass(new_class).css('overflow', 'hidden');
+        //$(document).unbind('wheel');
+        //setTimeout(function () { bindTools(); }, 1000);
+    }
+
     $('.block-tools-section-tablet button').bind('click', function () {
         if (!$(this).hasClass('active')) {
             $('.tools-tablet div').animate({'opacity': '0'}, 200);
             var dataName = $(this).parent().data('name');
-            console.log('click')
             if (dataName.toLowerCase() === 'camera') {
                 animateArrow('16.66%');
-                changeTool($('.icon-dji'), $('.icon-camera'), 'tools-dji', 'tools-camera', '1', false);
+                changeTooll($('.icon-dji'), $('.icon-camera'), 'tools-dji', 'tools-camera', '1', true);
             } else if (dataName.toLowerCase() === 'drone') {
                 animateArrow('50%');
-                changeTool($('.icon-camera'), $('.icon-dji'), 'tools-camera', 'tools-dji', '2', false);
+                changeTooll($('.icon-camera'), $('.icon-dji'), 'tools-camera', 'tools-dji', '2', false);
             } else {
                 animateArrow('83.33%');
-                changeTool($('.icon-dji'), $('.icon-movi'), 'tools-dji', 'tools-movi', '3', false);
+                changeTooll($('.icon-dji'), $('.icon-movi'), 'tools-dji', 'tools-movi', '3', false);
             }
 
             setTimeout(function () {
