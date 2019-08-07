fc15d046061a7ad25dfca5169bd9c94eccce5fbe
romkravets <romann.kravets@gmail.com>
2019-08-07 12:26:27 +0300

    Update fix.js

--- app/js/fix.js
+++ app/js/fix.js
@@ -154,7 +154,7 @@ $(document).ready(function () {
                                 $(window).unbind('wheel');
                             }
                         } else {
-                            windowHeight1 = windowHeight1 + event.deltaY * 3;
+                            windowHeight1 = windowHeight1 + event.deltaY;
                             if (windowHeight1 <= $(window).height()) {
                                  $('#section-instagram').css('pointer-events', 'auto');
                                 $('#fullpage').css('transform', 'translate3d(0px, -' + $(window).height() + 'px, 0px)');
@@ -163,7 +163,7 @@ $(document).ready(function () {
                             }
                         }
                     }
-                    else if ($('#section-instagram .fp-scroller').css('transform') === 'matrix(1, 0, 0, 1, 0, ' + 0 + ')') {
+                    if ($('#section-instagram .fp-scroller').css('transform') === 'matrix(1, 0, 0, 1, 0, ' + 0 + ')') {
                         $.fn.fullpage.setAllowScrolling(false);
                         var windowHeightTop = $(window).height();
                             $(window).bind('wheel', function (e) {
@@ -192,46 +192,8 @@ $(document).ready(function () {
                                         $(window).unbind('wheel');
                                     }
                                 }
-                    //     console.log(barr);
-                    //     var windowHeightTop = $(window).height();
-                    //     console.log(windowHeightTop)
-                    //     var sectAboutHeightTop = $('#section-tools').height();
-                    //     console.log(sectAboutHeightTop)
-                    //     var differSect1Top = windowHeightTop - sectAboutHeightTop;
-                         //$('#fullpage').css('transform', 'translate3d(0px,-2500px, 0px)');
-                         //$.fn.fullpage.setAllowScrolling(true);
-                    //     $(window).bind('wheel', function (e) {
-                    //         if ($('#section-instagram .fp-scroller').css('transform') === 'matrix(1, 0, 0, 1, 0, ' + 0 + ')') {
-                    //             var delta1 = e.originalEvent.deltaY;
-                    //            // $('#section-instagram').css('pointer-events', 'none');
-                    //             if (delta1 < 0) {
-                    //                 windowHeightTop = windowHeightTop + event.deltaY * 16;
-                    //                 //$('#fullpage').css('transform', 'translate3d(0px, topSecInsta, 0px)');
-                    //                 //$('#fullpage').css('transform', 'translate3d(0px, -' + windowHeightTop + 'px, 0px)');
-                    //                 if (windowHeight1 > $('#section-tools').outerHeight() / 100 * 20 + $(window).height()) {
-                    //                     $.fn.fullpage.setAllowScrolling(true);
-                    //                      $('#section-instagram').css('pointer-events', 'auto');
-                    //                     $(window).unbind('wheel');
-                    //                 }
-                    //             } else {
-                    //                 windowHeight1 = windowHeight1 + event.deltaY * 16;
-                    //                 if (windowHeight1 <= $(window).height()) {
-                    //                      $('#section-instagram').css('pointer-events', 'auto');
-                    //                     $('#fullpage').css('transform', 'translate3d(0px, -' + $(window).height() + 'px, 0px)');
-                    //                 } else {
-                    //                     $('#fullpage').css('transform', 'translate3d(0px, -' + windowHeight1 + 'px, 0px)');
-                    //                 }
-                    //             }
-                    //         }
-                    //     //$.fn.fullpage.setAllowScrolling(true);
-                    // // } else {
-                    // //     $.fn.fullpage.setAllowScrolling(false);
-                    // // }
-
-                        });
-                    } else {
-                        $.fn.fullpage.setAllowScrolling(false);
-                    }
+                            });
+                        }
                     if (insta == false) {
                         if (isDesktop) {
                             $("span.countup").html("1k");
@@ -271,7 +233,71 @@ $(document).ready(function () {
                             }, 3);
                         }
                     }
-                });
+                    });
+                //  } if ($('#section-instagram .fp-scroller').css('transform') === 'matrix(1, 0, 0, 1, 0, ' + 0 + ')') {
+                //     $.fn.fullpage.setAllowScrolling(false);
+                //     var windowHeightTop = $(window).height();
+                //         $(window).bind('wheel', function (e) {
+                //             var delta1 = e.originalEvent.deltaY;
+                //             if (delta1 > 0) {
+                //                 windowHeightTop = windowHeightTop + event.deltaY;
+                //                 //$('#fullpage').css('transform', 'translate3d(0px, topSecInsta, 0px)');
+                //                 //$('#fullpage').css('transform', 'translate3d(0px, -' + windowHeightTop + 'px, 0px)');
+                //                 $.fn.fullpage.setAllowScrolling(true);
+                //                 //$(window).unbind('wheel');
+                //                 // if (windowHeightTop > $('#section-tools').outerHeight() / 100 * 20 + $(window).height()) {
+                //                 //     $.fn.fullpage.setAllowScrolling(true);
+                //                 //         $('#section-instagram').css('pointer-events', 'auto');
+                //                 //     $(window).unbind('wheel');
+                //                 // }
+                //             } else {
+                //                 windowHeightTop = windowHeightTop + event.deltaY;
+                //                 if (windowHeightTop <= $(window).height()) {
+                //                         $('#section-instagram').css('pointer-events', 'auto');
+                //                     $('#fullpage').css('transform', 'translate3d(0px, -1600px, 0px)');
+                //                     $.fn.fullpage.setAllowScrolling(true);
+                //                     $(window).unbind('wheel');
+                //                 } else {
+                //                     $('#fullpage').css('transform', 'translate3d(0px, -' + windowHeightTop + 'px, 0px)');
+                //                     $.fn.fullpage.setAllowScrolling(true);
+                //                     $(window).unbind('wheel');
+                //                 }
+                //             }
+                //     console.log(barr);
+                //     var windowHeightTop = $(window).height();
+                //     console.log(windowHeightTop)
+                //     var sectAboutHeightTop = $('#section-tools').height();
+                //     console.log(sectAboutHeightTop)
+                //     var differSect1Top = windowHeightTop - sectAboutHeightTop;
+                     //$('#fullpage').css('transform', 'translate3d(0px,-2500px, 0px)');
+                     //$.fn.fullpage.setAllowScrolling(true);
+                //     $(window).bind('wheel', function (e) {
+                //         if ($('#section-instagram .fp-scroller').css('transform') === 'matrix(1, 0, 0, 1, 0, ' + 0 + ')') {
+                //             var delta1 = e.originalEvent.deltaY;
+                //            // $('#section-instagram').css('pointer-events', 'none');
+                //             if (delta1 < 0) {
+                //                 windowHeightTop = windowHeightTop + event.deltaY * 16;
+                //                 //$('#fullpage').css('transform', 'translate3d(0px, topSecInsta, 0px)');
+                //                 //$('#fullpage').css('transform', 'translate3d(0px, -' + windowHeightTop + 'px, 0px)');
+                //                 if (windowHeight1 > $('#section-tools').outerHeight() / 100 * 20 + $(window).height()) {
+                //                     $.fn.fullpage.setAllowScrolling(true);
+                //                      $('#section-instagram').css('pointer-events', 'auto');
+                //                     $(window).unbind('wheel');
+                //                 }
+                //             } else {
+                //                 windowHeight1 = windowHeight1 + event.deltaY * 16;
+                //                 if (windowHeight1 <= $(window).height()) {
+                //                      $('#section-instagram').css('pointer-events', 'auto');
+                //                     $('#fullpage').css('transform', 'translate3d(0px, -' + $(window).height() + 'px, 0px)');
+                //                 } else {
+                //                     $('#fullpage').css('transform', 'translate3d(0px, -' + windowHeight1 + 'px, 0px)');
+                //                 }
+                //             }
+                //         }
+                //     //$.fn.fullpage.setAllowScrolling(true);
+                // // } else {
+                // //     $.fn.fullpage.setAllowScrolling(false);
+                // // }
             } else {
                 dots[nextIndex.index].addClass('active');
             }
@@ -355,6 +381,7 @@ $(document).ready(function () {
             $.fn.fullpage.setResponsive(true);
         }
     });
+    //$.fn.fullpage.reBuild();
     $.fn.fullpage.setAllowScrolling(true);
 
     $('#menuToggle input, #menuToggle-mobile input').click(function () {
