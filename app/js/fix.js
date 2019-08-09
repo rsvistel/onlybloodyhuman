5e2f0ba59ae0af5401c87d2fed3a072364d1ba49
romkravets <romann.kravets@gmail.com>
2019-08-09 20:14:45 +0300

    Update fix.js

--- app/js/fix.js
+++ app/js/fix.js
@@ -245,13 +245,14 @@ $(document).ready(function () {
                                 $(window).unbind('wheel');
                             }
                         } else  {
-                            //sectionScroll = sectionScroll + event.deltaY;
-                            // if (sectionScroll <= $(window).height()) {
-                            //     sections[nextIndex.index].css('pointer-events', 'auto');
-                            //     $('#fullpage').css('transform', 'translate3d(0px, -' + sections[nextIndex.index].offset().top + 'px, 0px)');
-                            // } else {
-                            //     $('#fullpage').css('transform', 'translate3d(0px, -' + sectionScroll + 'px, 0px)');
-                            // }
+                           sectionScroll = sectionScroll + event.deltaY;
+                            if (sectionScroll >= $(window).height()) {
+                            console.log('bottom');
+                            sections[nextIndex.index].css('pointer-events', 'auto');
+                            $('#fullpage').css('transform', 'translate3d(0px, -' + sectionStartPosition + 'px, 0px)');
+                            } else {
+                                $('#fullpage').css('transform', 'translate3d(0px, -' + sectionScroll + 'px, 0px)');
+                            }
                         }
                     }
                 });
