4d043c451caa9acd1d3e6bb9c374e0802199b6f6
romkravets <romann.kravets@gmail.com>
2019-08-10 09:34:56 +0300

    Update fix.js

--- app/js/fix.js
+++ app/js/fix.js
@@ -247,7 +247,6 @@ $(document).ready(function () {
                         } else  {
                            sectionScroll = sectionScroll + event.deltaY;
                             if (sectionScroll >= $(window).height()) {
-                            console.log('bottom');
                             sections[nextIndex.index].css('pointer-events', 'auto');
                             $('#fullpage').css('transform', 'translate3d(0px, -' + sectionStartPosition + 'px, 0px)');
                             } else {
